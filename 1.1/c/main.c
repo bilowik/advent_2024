1597b0a0abb28291bbf9e0d35b6a38c8abb20f1d
bilowik <bilowik@bilowik.com>
2024-12-07 19:22:41 -0500

    Finish day 1 in C

--- 1.1/c/main.c
+++ 1.1/c/main.c
@@ -11,12 +11,16 @@ typedef struct List {
 
 
 int list_append(List *self, int value) {
-	self->values[self->len++] = value;
 	if(self->len == self->alloc_size) {
 		self->alloc_size *= 2;
 		self->values = realloc(self->values, sizeof(int) * self->alloc_size);
 	}
+	self->values[self->len++] = value;
 	return 0;
+}
+
+int list_clear(List *self) {
+	self->len = 0;
 }
 
 int list_init(List *self, int initial_size) {
@@ -25,35 +29,32 @@ int list_init(List *self, int initial_size) {
 	self->len = 0;
 }
 
-// Parses lists_in and places pointers to the two parsed lists 
-// - lists_out will be updated with a pointer to an array of lists. Caller owns the memory and needs to free it.
-// - lists_count will be updated with the number of lists.
-// If an error occurs, the output pointers will be unchanged.
-int parse_lists(char *lists_in, int lists_in_len, List **lists_out, int *list_count_out) {
+/// Destroys the given list, unallocating memory for the internal list.
+/// self is still owned by the calling function and is responsible for that memory.
+/// Usage after list_destroy is called is undefined.
+int list_destroy(List *self) {
+	free(self->values);
+}
+
+
+// Returns a malloc'd list owned by the caller of this function.
+// The given list wlil have each entry correspond to the given column.
+// line is assumed to be a null-terminated string.
+// list is assumed to be empty but initialized.
+List *parse_line(char *line, List *list) {
 	char buffer[256];
-	int column_counter = 0;
-	int row_counter = 0;
-	char current_char = 0;
+	char curr_char = 0;
 	int buffer_count = 0;
 	int state = 0;
-	int num_lists = 1;
-	int curr_int;
+	int curr_int = 0;
 
-	List *lists = malloc(sizeof(List) * num_lists);
+	int line_len = strlen(line);
 
-	for(int i = 0; i < num_lists; i++) {
-		list_init(&lists[i], 32);
-	}
-	
-	// State:
-	// 	0: Last charactor was a character
-	// 	1: Last character was whitespace
-	// 	2: Last character was newline
-	//
-	// In state 1, all input is ignored until new characters are found.
-	for(int i = 0; i < lists_in_len; i++) {
-		current_char = lists_in[i];
-		switch(current_char) {
+	for(int i = 0; i < line_len; i++) {
+		curr_char = line[i];
+		switch(curr_char) {
+			case '\n':
+				// The final char in the line
 			case ' ':
 				if(state == 0) {
 					// We've gone from characters to whitespace.
@@ -61,45 +62,69 @@ int parse_lists(char *lists_in, int lists_in_len, List **lists_out, int *list_co
 					state = 1;
 					buffer[buffer_count] = 0; // null-term for atoi
 					curr_int = atoi(buffer);
-					list_append(lists + column_counter, curr_int);
+					list_append(list, curr_int);
 					buffer_count = 0;
-					column_counter++;
 				}
-				break;
-			case '\n':
-			case '\r':
-				// We've reached the end of the line, we 
-				// finish up the final column.
-				if(state < 2) { // state 0 or 1.
-					// We were not in a whitespcae or newline state
-					// so save what we have currently.
-					state = 2;
-					if(buffer_count > 0) {
-						// There's actual stuff in the buffer.
-						buffer[buffer_count] = 0;
-						curr_int = atoi(buffer);
-						list_append(lists + column_counter, curr_int);
-					}
-					column_counter = 0;
-				}
+				// else, we just break until we get something that isn't whitespace.
 				break;
 			default:
-				if((state != 0) && (column_counter >= num_lists)) {
-					// We need to reallocate.
-					// Have to do it here bc we only
-					// want to realloc if we hit whitespace
-					// followed by non-newline chars.
-					lists = realloc(lists, sizeof(int *) * ++num_lists);
-				}
 				// This is any other character.
 				state = 0; // if we aren't already
-				buffer[buffer_count++] = current_char;
+				buffer[buffer_count++] = curr_char;
 		}
 	}
+	return list;
+}
+
+// Parses the given file, updating lists_out with a pointer to an array
+// of lists, and lists_count_out with the length of that array.
+int parse_file(FILE *f, List **lists_out, int *list_count_out) {
+	char buffer[1024];
+	int num_lists = 1;
+	int lists_size = 1;
+	int lists_size_diff = 0;
+	int lists_len_diff = 0;
+
+	List curr_list;
+	List *lists = malloc(sizeof(List) * lists_size);
+	for(int i = 0; i < lists_size; i++) {
+		list_init(lists + i, 64);
+	}
+	list_init(&curr_list, 64);
+	
+
+	while(fgets(buffer, sizeof(buffer), f) != NULL) {
+		parse_line(buffer, &curr_list);
+
+		lists_size_diff = curr_list.len - lists_size;
+
+		if(curr_list.len > lists_size) {
+			// We need to reallocate
+			lists_size *= 2;
+			lists = realloc(lists, lists_size * sizeof(List));
+		}
+		while(num_lists < curr_list.len) {
+			// Increment num_lists and init additional
+			// lists.
+			list_init(lists + (num_lists++), 64);
+		}
+
+		for(int i = 0; i < curr_list.len; i++) {
+			list_append(lists + i, curr_list.values[i]);
+		}
+		list_clear(&curr_list);
+	}
+
+
 
 	*lists_out = lists;
 	*list_count_out = num_lists;
+	list_destroy(&curr_list);	
 	return 0;
+}
+
+int int_compare_func(const void *lhs, const void *rhs) {
+	return *(int *) lhs - *(int *) rhs;
 }
 
 int main(int argc, char **argv) {
@@ -107,15 +132,43 @@ int main(int argc, char **argv) {
 		printf("Missing argument. Usage: main <FILE_PATH>");
 		exit(-1);
 	}
+	FILE *f = fopen(argv[1], "r");
+	if(f == NULL) {
+		// Fialed to open file
+		perror("Failed to open file");
+		return -1;
+	}
 
-	List *lists; 
-	char *path = argv[0];
+	List *lists;
+	int num_lists = 0;
 
-	FILE *f = fopen(path, "r");
-	fseek(f, 0, SEEK_END);
-
-
+	int result = parse_file(f, &lists, &num_lists);
+	
 	fclose(f);
 
+	for(int i = 0; i < num_lists; i++) {
+		qsort(lists[i].values, lists[i].len, sizeof(int), &int_compare_func);
+	}
 
+	if(num_lists != 2) {
+		printf("Got more lists than expected. Expected 2, got %d\n", num_lists);
+		return -1;
+	}
+
+	if(lists[0].len != lists[1].len) {
+		printf("Length of each list is not equal! List 1: %d; List 2: %d", lists[0].len, lists[1].len);
+		return -2;
+	}
+	
+	int distance = 0;
+	for(int i = 0; i < lists[0].len; i++) {
+		distance += abs(lists[0].values[i] - lists[1].values[i]);
+	}
+
+	printf("%d\n", distance);
+
+	for(int i = 0; i < num_lists; i++) {
+		list_destroy(lists + i);
+	}
+	free(lists);
 }
