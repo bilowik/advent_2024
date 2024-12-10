7952e0189ad14a86b326684a78fac0e0dfb3a8b1
bilowik <bilowik@bilowik.com>
2024-12-09 20:10:07 -0500

    Finish part 2 of day 2

--- 2/c/main.c
+++ 2/c/main.c
@@ -12,6 +12,7 @@ struct Node {
 
 typedef struct LinkedList {
 	Node *head;
+	int len;
 } LinkedList;
 
 int node_init(Node *self, int value) {
@@ -27,6 +28,7 @@ int linked_list_append(LinkedList *self, int value) {
 	}
 	*node_ptr = malloc(sizeof(Node));
 	node_init(*node_ptr, value);
+	self->len++;
 
 	return 0;
 }
@@ -34,14 +36,18 @@ int linked_list_append(LinkedList *self, int value) {
 
 int linked_list_init(LinkedList *self) {
 	self->head = NULL;
+	self->len = 0;
 	return 0;
 }
 
 // Returns the Node with the given idx, or NULL if idx is out of bounds.
 // The linked list retains ownership.
 Node *linked_list_get(LinkedList *self, int idx) {
+	if(idx >= self->len) {
+		return NULL;
+	}
 	Node *node_ptr = self->head;
-	for(int i = 1; i < idx; i++) {
+	for(int i = 0; i < idx; i++) {
 		node_ptr = node_ptr->next;
 
 		if(node_ptr == NULL) {
@@ -78,6 +84,7 @@ int linked_list_empty(LinkedList *self) {
 		curr_node = next_node;
 	}
 	self->head = NULL;
+	self->len = 0;
 	return 0;
 }
 
@@ -127,16 +134,59 @@ int sign(int value) {
 	return 0;
 }
 
+void print_list(LinkedList *list) {
+	Node *curr_node = list->head;
+	printf("-> ");
+	while(curr_node != NULL) {
+		printf("%d, ", curr_node->value);
+		curr_node = curr_node->next;
+	}
+	printf("\n");
+}
+
+
+int report_is_safe(LinkedList *list) {
+	Node *curr_node = list->head;
+	int is_safe = 1;
+	int diff = 0;
+
+	// Need to initialize this here .
+	int prev_change_direction = sign(curr_node->value - curr_node->next->value);
+	do {
+
+		diff = curr_node->value - curr_node->next->value;
+		is_safe = ((abs(diff) <= 3) && diff != 0) && (sign(diff) == prev_change_direction);
+
+		prev_change_direction = sign(diff);
+		curr_node = curr_node->next;
+
+	} while((curr_node->next != NULL) && is_safe);
+
+	return is_safe;
+}
+
+int report_is_safe_dampened(LinkedList *list) {
+	int is_safe = 0;
+	LinkedList spliced;
+	linked_list_init(&spliced);
+	for(int i = 0; i < list->len; i++) {
+		for(int j = 0; j < list->len; j++) {
+			if(j != i) {
+				linked_list_append(&spliced, linked_list_get(list, j)->value);
+			}
+		}
+		is_safe = is_safe || report_is_safe(&spliced); 
+		linked_list_empty(&spliced);
+	}
+
+	return is_safe;
+}
+
 int main(int argc, char *argv[]) {
 	FILE *input;
 	char buffer[1024];
-	int prev_value;
-	int diff; // <0 = decreasing, >0 = increase, 0 = unknown yet
-	int prev_change_direction;
-	int is_valid = 0;
-	int valid_count_2;
-	int valid_count = 0;
-	int curr_diff = 0;
+	int dampened_safe_count = 0;
+	int safe_count = 0;
 	Node *curr_node;
 	LinkedList curr_line;
 	linked_list_init(&curr_line);
@@ -153,32 +203,16 @@ int main(int argc, char *argv[]) {
 	// We do not need to init curr_line.
 
 	while(fgets(buffer, sizeof(buffer), input) != NULL) {
-		is_valid = 1;
 		parse_line(buffer, &curr_line);
-
-
-		curr_node = curr_line.head;
-
-		// Need to initialize this here .
-		prev_change_direction = sign(curr_node->value - curr_node->next->value);
-		do {
-
-			diff = curr_node->value - curr_node->next->value;
-			is_valid = ((abs(diff) <= 3) && diff != 0) && (sign(diff) == prev_change_direction);
-
-			prev_change_direction = sign(diff);
-			curr_node = curr_node->next;
-
-		} while((curr_node->next != NULL) && is_valid);
-
-		if(is_valid) {
-			valid_count++;
+		if (report_is_safe_dampened(&curr_line)) {
+			dampened_safe_count++;
+			if (report_is_safe(&curr_line)) safe_count++;
 		}
 		linked_list_empty(&curr_line);
 	}
 	fclose(input);
-	printf("%d\n", valid_count);
-	printf("%d\n", valid_count_2);
+	printf("%d\n", safe_count);
+	printf("%d\n", dampened_safe_count);
 	return 0;
 }
 
