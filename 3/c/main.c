8564be16e756c3cbfc1d2e79652c530d8e5f3996
bilowik <bilowik@bilowik.com>
2024-12-12 20:59:50 -0500

    Finish day 3 part 2

--- 3/c/main.c
+++ 3/c/main.c
@@ -18,8 +18,10 @@ int main(int argc, char *argv[]) {
 	int read_chunk_idx = 0; // Tracks iteration in the read_chunk
 	int curr_num = 0; // Stores the parsed number currently being read.
 	int curr_product = 1; // Stores the curent output of the given mul op.
-	int total_sum = 0; // The sum of the multiply operations.
+	int total_sum = 0; // The sum of only enabled multiply operations.
+	int total_sum_with_disabled = 0; // The sum of ALL multiply operations
 	int arg_count = 0; // Counts the number of args in the current mul op.	
+	int mul_enabled = 1; // Tracks if mul operations should be ignored.
 
 	// States: 
 	//
@@ -29,6 +31,13 @@ int main(int argc, char *argv[]) {
 	// 3: Last char was 'l'
 	// 4: Last char was '(' or ','
 	// 5: Last char was [0-9]
+	// 11: Last char was d
+	// 12: Last char was o
+	// 13: Last char was n
+	// 14: Last char was '
+	// 15: Last char was t
+	// 16: Last char was ( following state 12.
+	// 17: Last char was ( following state 15.
 	int state = 0;
 
 	if((f = fopen(argv[1], "r")) == NULL) {
@@ -40,10 +49,19 @@ int main(int argc, char *argv[]) {
 		read_chunk_idx = 0;
 		while((curr_char = read_chunk[read_chunk_idx++]) != '\0') {
 
+			if(curr_char == 'm') {
+				// Skip the following check switch
+				state = 1;
+				continue;
+			}
+			else if(curr_char == 'd') {
+				//same as above.
+				state = 11;
+				continue;
+			}
 			switch(state) {
 				case 0:
-					state = (curr_char == 'm') ? 1 : 0;
-					break;
+					break; // Handled above.
 				case 1:
 					state = (curr_char == 'u') ? 2 : 0;
 					break;
@@ -85,7 +103,8 @@ int main(int argc, char *argv[]) {
 						// We've reached the end of this mul op and
 						// it is valid. Go back to state 0.
 						curr_product *= curr_num;
-						total_sum += curr_product;
+						total_sum += mul_enabled ? curr_product : 0;
+						total_sum_with_disabled += curr_product;
 						state = 0;
 					}
 					else {
@@ -102,13 +121,52 @@ int main(int argc, char *argv[]) {
 						curr_product = 1;
 					}
 					break;
-
+				case 11:
+					state = (curr_char == 'o') ? 12 : 0;
+					break;
+				case 12:
+					switch(curr_char) {
+						case 'n':
+							state = 13;
+							break;
+						case '(':
+							state = 16;
+							break;
+						default:
+							state = 0;
+							break;
+					}
+					break;
+				case 13:
+					state = (curr_char == '\'') ? 14 : 0;
+					break;
+				case 14:
+					state = (curr_char == 't') ? 15 : 0;
+					break;
+				case 15:
+					state = (curr_char == '(') ? 17: 0;
+					break;
+				case 16: 
+					if(curr_char == ')') {
+						// We've encountered a valid do() command.
+						mul_enabled = 1;
+					}
+					state = 0;
+					break;
+				case 17:
+					if(curr_char == ')') {
+						// We've encountered a valid don't command.
+						mul_enabled = 0;
+					}
+					state = 0;
+					break;
 			}
 		}
 	}
 
 	fclose(f);
 
+	printf("%d\n", total_sum_with_disabled);
 	printf("%d\n", total_sum);
 	return 0;
 }
